d9d99c64eeda48bb09433372e3b508dda2f5e5dc
Deven Sitapara <shriramsoft@gmail.com>
2021-03-13 19:50:04 +0530

    edit

---
+++ client/src/helper/grid-functions.js
@@ -0,0 +1,33 @@
+import _ from 'underscore';
+
+
+function dataTableRowtoJson(row) {
+
+    if (_.isUndefined(row)) {
+        return {};
+    }
+    // console.log("row");
+    // console.log(row);
+    // console.log("_.isUndefined(row.cells)");
+    // console.log(_.isUndefined(row.cells));
+    // console.log("typeof(row.cells)");
+    // console.log(typeof (row.cells) === "undefined");
+
+    var data = [];
+    if (!_.isUndefined(row.cells) && row.cells.length > 0) {
+
+        row.cells.forEach((element) => {
+
+            if (!_.isUndefined(element) && !_.isUndefined(element.info.header) && !_.isUndefined(element.value)) {
+
+                data[element.info.header] = element.value
+            }
+
+        });
+        return data;
+    }
+
+
+}
+
+export { dataTableRowtoJson }
